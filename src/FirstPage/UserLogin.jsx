90819df57623c302d5e5e044c2783cbcd4640d64
Richa3003 <richagupta3003@gmail.com>
2023-03-26 14:44:38 +0530

    Login page changes

--- src/FirstPage/UserLogin.jsx
+++ src/FirstPage/UserLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import FirstPage from ".";
-// import "./Login.css";
+import './Login.css';
 // import '../App.css'
 
 const UserLogin =()=>{
@@ -15,36 +15,54 @@ const UserLogin =()=>{
         e.preventDefault()
         const newEntry ={email:email, password:password};
         setAllEntry([...allEntry, newEntry]);
-        console.log(allEntry);   
+        console.log(allEntry);  
         setUserData(newEntry)    
         // setIsLoggedIn(true);
         allEntry.length>0? setIsLoggedIn(true) : setIsLoggedIn(false)
         // navigate("/")
-    }
+    };
 
     const logOutButton = () =>{
-        console.log("Signut")
-    }
+        console.log("Signout")
+    };
+    
+
     return(
         isLoggedIn ?  
         <div><FirstPage allEntry={userData} setIsLoggedIn={setIsLoggedIn}/></div> :
-        <div className="App-header">
-        <div className="formStyle">
+        
+            <div className="App-header">
+            <div className="formStyle">
+            <div className="form-box">
+            <h1>Sign In</h1>
         <form action="" onSubmit={submitForm}>
-           <div> <label className="htmlFor" htmlFor="email">Email</label>
-            <input className="inputfield" type="name" name="name" id="name" autoComplete="off" value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-            /></div>
-            <div> <label className="htmlFor" htmlFor="password">Password</label>
-            <input className="inputfield" name="password" id="password" type="password" autoComplete="off" value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-            /></div>
-
-            <div><button className="button" type="submit">UserLogin</button>
+           <div className="input-group">
+           {/* <div> 
+               <input className="inputfield" id="nameField" type= "text" placeholder="Name"/>
+           </div> */}
+           <div> 
+                {/* <label className="htmlFor" htmlFor="email">Email</label> */}
+                <input className="inputfield" type="email" name="email"  placeholder="Email" id="email" autoComplete="off" value={email}
+                 onChange={(e)=>setEmail(e.target.value)}/>
+           </div>
+           <div> 
+                {/* <label className="htmlFor" htmlFor="password">Password</label> */}
+                <input className="inputfield" name="password" id="password" type="password" placeholder="Password" autoComplete="off" value={password}
+                 onChange={(e)=>setPassword(e.target.value)}/>
+           </div>
+            <div class="btn-field">
+            <button classname="button" type="submit"  class="disable" id="signinbtn" >Sign In</button>
+                <button type="submit"  class="disable" id="signupbtn">Sign Up</button>
             {/* <button className="button" onClick={logOutButton}>SingUp</button> */}
             </div>
-        </form></div>
-       
+            <p class="forget-password"><a href="#"><u>Forgotten Password?</u></a></p>
+
+            </div>
+        </form>
+        </div>
+    </div>
+ 
+  
 {/* 
             {allEntry.map((currData)=>{
                 return (
@@ -56,7 +74,8 @@ const UserLogin =()=>{
             })
             }
          */}
-        </div>
-    )
-}
+</div>
+
+    );
+};
 export default UserLogin;
